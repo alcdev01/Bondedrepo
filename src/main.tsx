db2abb162bbdb862deae41a4b395f8029cc31f82
aspad <aspad@mail.com>
2026-08-14 20:30:47 +0545

    Fix GitHub Pages white screen

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import '../assets/site.css';
 import '../assets/site-repair.css';
@@ -9,4 +9,4 @@ import '../assets/redesign-fixes.css';
 
 const root = document.getElementById('root');
 if (!root) throw new Error('Root element was not found.');
-createRoot(root).render(<React.StrictMode><BrowserRouter><App /></BrowserRouter></React.StrictMode>);
+createRoot(root).render(<React.StrictMode><HashRouter><App /></HashRouter></React.StrictMode>);
